01d75123970b4267a37036ae94f49b861d927691
thiago amaral julio <amaralt778@gmail.com>
2026-01-31 18:21:43 -0300

    subindo arquivos novos

---
+++ index.js/aula1.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML=("meu primeiro texto");
+document.write("escrevendo com document write");
+console.log("isso é um console log");
--- indexjs/aulas1.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("texto").innerHTML="meu primeiro texto <b>JS<b>";
-document.write('oi estou escrevendo com document.write')
