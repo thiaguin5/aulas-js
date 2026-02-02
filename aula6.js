15b6d7104b761cc1213cfc20df3e8e1fc7137f67
thiago amaral julio <amaralt778@gmail.com>
2026-02-02 19:58:39 -0300

    subindo variaveis var

---
+++ aula6.js
@@ -0,0 +1,15 @@
+//variaveis
+
+//var
+//const
+//let
+
+var pote= "bombom";
+
+
+
+var a = 7;
+var b = 9;
+var c = a + b;
+
+alert(c);
--- index.js/aula1.js
+++ index.js/aula1.js
@@ -1,3 +1,4 @@
 document.getElementById("texto").innerHTML=("meu primeiro texto");
 document.write("escrevendo com document write");
 console.log("isso é um console log");
+
